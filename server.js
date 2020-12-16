6aefc9cf6b379466e8e7fdfe2a0392845c5c5ac6
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-12-16 09:16:40 -0800

    added static folder in server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('public'))
+
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
