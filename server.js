54d605e54e41b0ace938adb9a81f46bf3d2f051c
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2020-12-15 22:59:12 -0800

    added get request for html pages

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 })
+app.use('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
